af057e24bda278bf6deab7ee684c76a0e28d2d6a
Juan E Quintero R <juequinterore666@gmail.com>
2021-12-14 08:57:24 -0500

    feat: add swappable_main_screen.dart and swappable_control_pad.dart implementations

---
+++ lib/2/swappable_control_pad.dart
@@ -0,0 +1,105 @@
+import 'dart:math';
+
+import 'package:command_pattern/2/base_command.dart';
+import 'package:flutter/material.dart';
+
+const double eighthRotation = pi / 4;
+
+class SecondControlPad extends StatelessWidget {
+  final BaseCommand aCommand;
+  final BaseCommand bCommand;
+  final BaseCommand xCommand;
+  final BaseCommand yCommand;
+
+  const SecondControlPad({
+    this.aCommand = NullCommand.instance,
+    this.bCommand = NullCommand.instance,
+    this.xCommand = NullCommand.instance,
+    this.yCommand = NullCommand.instance,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Transform.rotate(
+        angle: -eighthRotation,
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  PadButton(
+                    text: "Y",
+                    color: const Color.fromRGBO(63, 141, 110, 1),
+                    onPressed: yCommand.execute,
+                  ),
+                  buttonsSpace,
+                  PadButton(
+                    text: "X",
+                    color: const Color.fromRGBO(31, 76, 157, 1),
+                    onPressed: yCommand.execute,
+                  )
+                ],
+              ),
+              buttonsSpace,
+              Row(
+                children: [
+                  PadButton(
+                    text: "B",
+                    color: const Color.fromRGBO(205, 176, 64, 1),
+                    onPressed: bCommand.execute,
+                  ),
+                  buttonsSpace,
+                  PadButton(
+                    text: "A",
+                    color: const Color.fromRGBO(212, 54, 46, 1),
+                    onPressed: aCommand.execute,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class PadButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final void Function() onPressed;
+
+  const PadButton({
+    required this.text,
+    required this.color,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 70,
+      height: 70,
+      child: MaterialButton(
+        shape: const CircleBorder(),
+        color: color,
+        child: Transform.rotate(
+          angle: eighthRotation,
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+const buttonsSpace = SizedBox(
+  height: 10,
+  width: 10,
+);
---
+++ lib/2/swappable_main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:command_pattern/2/base_command.dart';
+import 'package:command_pattern/2/commands/duck_command.dart';
+import 'package:command_pattern/2/commands/jump_command.dart';
+import 'package:command_pattern/2/commands/shoot_command.dart';
+import 'package:command_pattern/2/commands/swap_weapon_command.dart';
+import 'package:command_pattern/2/swappable_control_pad.dart';
+import 'package:flutter/material.dart';
+
+class SecondMainScreen extends StatefulWidget {
+  const SecondMainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondMainScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondMainScreen> {
+  late final BaseCommand aCommand;
+  late final BaseCommand bCommand;
+  late final BaseCommand xCommand;
+  late final BaseCommand yCommand;
+
+  String text = '';
+
+  _SecondScreenState() {
+    _readButtonsSetup();
+  }
+
+  void _readButtonsSetup() {
+    void printer(String action) {
+      setState(() {
+        text = action;
+      });
+    }
+
+    aCommand = DuckCommand(printer);
+    bCommand = JumpCommand(printer);
+    xCommand = ShootCommand(printer);
+    yCommand = SwapWeaponCommand(printer);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Control Pad 2"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(fontSize: 40),
+            ),
+            SecondControlPad(
+              aCommand: aCommand,
+              bCommand: bCommand,
+              xCommand: xCommand,
+              yCommand: yCommand,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
